986273f682e5944d90c114849d910481c0c96af4
manikbhatia5 <103627725+manikbhatia5@users.noreply.github.com>
2022-05-16 15:16:33 +0530

    new

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Hungry-
+# hungryCaptain
---
+++ app.js
@@ -0,0 +1,125 @@
+
+var candy = document.querySelector("#candy");
+var cc = document.querySelector("#cc");
+candy.addEventListener('click', function() {
+    var div
+console.log("button clicked");
+count++;
+
+cc.innerText = `Total = ${count}`;
+});
+var candy = document.querySelector("#candy1");
+var cc = document.querySelector("#cc1");
+candy.addEventListener('click', function() {
+    var div
+console.log("button clicked");
+count--;
+
+cc.innerText = `Total = ${count}`;
+});
+var gulab = document.querySelector("#gulab");
+var gc = document.querySelector("#gc");
+gulab.addEventListener('click', function() {
+    var div
+console.log("button clicked");
+count++;
+
+gc.innerText = `total = ${count}`;
+});
+var gulab = document.querySelector("#gulab1");
+var gc = document.querySelector("#gc1");
+gulab.addEventListener('click', function() {
+    var div
+count--;
+
+gc.innerText = `total = ${count}`;
+});
+
+var hotdog = document.querySelector("#hotdog");
+var hd = document.querySelector("#hd");
+hotdog.addEventListener('click', function() {
+    var div
+console.log("button clicked");
+count++;
+
+hd.innerText = `total = ${count}`;
+});
+var hotdog = document.querySelector("#hotdog1");
+var hd = document.querySelector("#hd1");
+hotdog.addEventListener('click', function() {
+    var div
+count--;
+
+hd1.innerText = `total = ${count}`;
+});
+
+var pizza = document.querySelector("#pizza");
+var pz = document.querySelector("#pz");
+pizza.addEventListener('click', function() {
+    var div
+console.log("button clicked");
+count++;
+
+pz.innerText = `total = ${count}`;
+});
+var pizza= document.querySelector("#pizza1");
+var pz = document.querySelector("#pz1");
+pizza.addEventListener('click', function() {
+    var div
+count--;
+
+pz1.innerText = `total = ${count}`;
+});
+
+var burger = document.querySelector("#burger");
+var bg = document.querySelector("#bg");
+burger.addEventListener('click', function() {
+    var div
+console.log("button clicked");
+count++;
+
+bg.innerText = `total = ${count}`;
+});
+var burger= document.querySelector("#burger1");
+var bg = document.querySelector("#bg1");
+burger.addEventListener('click', function() {
+    var div
+count--;
+bg.innerText = `total = ${count}`;
+});
+
+var oreo = document.querySelector("#oreo");
+var or = document.querySelector("#or");
+oreo.addEventListener('click', function() {
+    var div
+console.log("button clicked");
+count++;
+
+or.innerText = `total = ${count}`;
+});
+var oreo= document.querySelector("#oreo1");
+var or = document.querySelector("#or1");
+oreo.addEventListener('click', function() {
+    var div
+count--;
+or1.innerText = `total = ${count}`;
+});
+
+
+var choco = document.querySelector("#choco");
+var ch = document.querySelector("#ch");
+choco.addEventListener('click', function() {
+    var div
+console.log("button clicked");
+count++;
+
+ch.innerText = `total = ${count}`;
+});
+var choco= document.querySelector("#choco1");
+var ch = document.querySelector("#ch1");
+choco.addEventListener('click', function() {
+    var div
+count--;
+ch1.innerText = `total = ${count}`;
+});
+
